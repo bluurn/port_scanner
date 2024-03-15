82a47f8f806f6e920e8907d937b97d2607c37c46
vladimir.suvorov <vladimir.suvorov@xempus.com>
2024-03-15 12:08:37 +0100

    const

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use bpaf::Bpaf;
 use std::net::{IpAddr, Ipv4Addr};
 
-static DEFAULT_IP: std::net::Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+const DEFAULT_IP: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
 
 #[derive(Debug, Clone, Bpaf)]
 #[bpaf(options, version)]
 /// Accepts IPv4 address and number of threads
 pub struct Config {
     /// IPv4 address, defaults to 127.0.0.1
-    #[bpaf(short, long, fallback(std::net::IpAddr::V4(DEFAULT_IP)))]
+    #[bpaf(short, long, fallback(IpAddr::V4(DEFAULT_IP)))]
     pub ipaddr: IpAddr,
     /// Number of threads, defaults to 8
     #[bpaf(short, long, fallback(8), guard(positive, "must be positive"))]
